b15c756f508f115cddfabc735863a398c8501cf7
maksy <maksym.petliuk@gmail.com>
2025-10-06 14:06:42 +0300

    “magic numbers”

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,9 @@
 package mate.academy
 
+private const val CENTURY_OFFSET = 99
+private const val CENTURY_DIVISOR = 100
+
 fun getCentury(year: Int) : Int {
-    return (year + 99) / 100
+    return (year + CENTURY_OFFSET) / CENTURY_DIVISOR
 }
+
